a1a7567213051cd29d09b7281091d9f2ed79b746
obsolescenceL <384099319@qq.com>
2015-01-04 16:34:34 +0800

    add

---
+++ Prime/Eeuler.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+    > File Name: Eeuler.cpp
+    > Author: obsolescence
+    > Mail: 384099319@qq.com 
+    > Created Time: 2015年01月 4日 16:28:52
+ ************************************************************************/
+#include<cstdio>
+const int MAXN=31623;
+bool flag[MAXN];
+int prime[MAXN/3],k=0;
+void sieve(){
+  for(int i=2;i<MAXN;i++){
+    if(!flag[i])prime[k++]=i;
+    for(int j=0;i*prime[j]<MAXN;j++){
+      flag[i*prime[j]]=1;
+      if(i%prime[j]==0)break;
+    }
+  }
+}
+int main(){
+  int n,i,j;
+  sieve();
+  while(~scanf("%d",&n)&&n){
+    int ans=n;
+    for(i=0;i<k&&n>1;i++){
+      if(n%prime[i]==0){
+        ans=ans/prime[i]*(prime[i]-1);
+        while(n%prime[i]==0)n/=prime[i];
+      }
+    }
+    if(n>1)ans=ans/n*(n-1);
+    printf("%d\n",ans);
+  }
+}
