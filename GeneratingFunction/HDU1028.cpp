d87790ec6777481e22e29ac4016229e9ddadd009
obsolescenceL <384099319@qq.com>
2015-01-06 11:58:57 +0800

    add

--- GeneratingFunction/HDU1028.cpp
+++ GeneratingFunction/HDU1028.cpp
@@ -5,18 +5,18 @@
     > Created Time: 2015年01月 6日 10:54:56
  ************************************************************************/
 #include<cstdio>
-const int MAXN=120;
+const int MAXN=121;
 int c1[MAXN],c2[MAXN];
 int main(){
   int n,i,j,k;
   while(~scanf("%d",&n)){
     for(i=0;i<=n;i++)c1[i]=1,c2[i]=0;
     for(i=2;i<=n;i++){
-      for(j=0;j<=n;j++)
+      for(j=0;j<=n;j++){
         for(k=0;k+j<=n;k+=i)
           c2[k+j]+=c1[j];
-      for(j=0;j<=n;j++)
         c1[j]=c2[j],c2[j]=0;
+      }
     }
     printf("%d\n",c1[n]);
   }
