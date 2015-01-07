310e2e548dd6601d87dada370d43cf02a792d48d
obsolescenceL <384099319@qq.com>
2015-01-07 10:27:10 +0800

    add

--- GeneratingFunction/HDU2152.cpp
+++ GeneratingFunction/HDU2152.cpp
@@ -6,7 +6,7 @@
  ************************************************************************/
 #include<cstring>
 #include<cstdio>
-const int MAXN=10010;
+const int MAXN=110;
 int c1[MAXN],c2[MAXN],a[110],b[110],n,m;
 int main(){
   int i,j,k;
@@ -15,14 +15,12 @@ int main(){
     memset(c2,0,sizeof(c2));
     for(i=1;i<=n;i++)scanf("%d%d",a+i,b+i);
     for(i=a[1];i<=b[1];i++)c1[i]=1;
-    for(i=2;i<=n;i++){
+    for(i=2;i<=n;i++)
       for(j=0;j<=m;j++){
         for(k=a[i];j+k<=m && k<=b[i];k++)
           c2[j+k]+=c1[j];
-      }
-      for(j=0;j<=m;j++)
         c1[j]=c2[j],c2[j]=0;
-    }
+      }
     printf("%d\n",c1[m]);
   }
 }
