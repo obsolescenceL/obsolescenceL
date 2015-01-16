e4c8bea5dd1f0457cd0833277e35ac870a84797f
obsolescenceL <384099319@qq.com>
2015-01-16 11:30:43 +0800

    add

--- CatalanNumber/HDU1023.cpp
+++ CatalanNumber/HDU1023.cpp
@@ -12,33 +12,39 @@ const int MAXN=200;
 int n;
 struct bign{
   int len,s[MAXN];
+
   bign(){
     memset(s,0,sizeof s);
     len=1;
   }
+  
   bign(int num){
     *this=num;
   }
+
   bign(const char* num){
     *this=num;
   }
+
   bign operator = (int num){
     char s[MAXN];
     sprintf(s,"%d",num);
     *this=s;
     return *this;
   }
+
   bign operator = (const char* num){
     len=strlen(num);
     for(int i=0;i<len;i++)s[i]=num[len-i-1]-'0';
     return *this;
   }
+
   string str()const{
     string res="";
     for(int i=0;i<len;i++)res=(char)(s[i]+'0')+res;
-    if(res=="")res="0";
     return res;
   }
+
   bign operator + (const bign& b)const{
     bign c;
     c.len=0;
@@ -51,51 +57,54 @@ struct bign{
     }
     return c;
   }
+
   void clean(){
     while(len>1 && !s[len-1])len--;
   }
+
   bign operator * (const bign& b){
-    bign c; c.len=len+b.len;
+    bign c;
+    c.len=len+b.len;
     for(int i=0;i<len;i++)
       for(int j=0;j<b.len;j++)
         c.s[i+j]+=s[i]*b.s[j];
-    for(int i=0;i<c.len-1;i++)
-      c.s[i+1]+=c.s[i]/10, c.s[i]%=10;
+    for(int i=0;i<c.len-1;i++){
+      c.s[i+1]+=c.s[i]/10;
+      c.s[i]%=10;
+    }
     c.clean();
     return c;
   }
-  bign operator / (const bign& b){
-
-
-
-
-
-
-
-
 
   bign operator += (const bign& b){
     *this=*this+b;
     return *this;
   }
 };
+
 istream& operator >> (istream& in,bign& x){
   string s;
   in>>s;
   x=s.c_str();
   return in;
 }
+
 ostream& operator << (ostream& out,const bign& x){
   out<<x.str();
   return out;
 }
+
 bign h[110];
 void Catalan(){
   h[0]=1;h[1]=1;
   for(int i=2;i<101;i++)
-    h[i]=h[i-1]*(4*i-2)/(i+1);
+    for(int j=0;j<i;j++)
+      h[i]+=h[j]*h[i-j-1];
 }
+
 int main(){
   Catalan();
-  while(cin>>n)cout<<h[n]<<endl;
+  while(cin>>n){
+    cout<<h[n]<<endl;
+  }
 }
