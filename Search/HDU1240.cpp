ea2a5da31a76bd04e039c6db0beb48a28f76759b
obsolescenceL <384099319@qq.com>
2015-01-20 16:51:09 +0800

    add

--- Search/HDU1240.cpp
+++ Search/HDU1240.cpp
@@ -1,11 +1,3 @@
-/*************************************************************************
-     File Name: HDU1240.cpp
-     ID: obsoles1
-     LANG: C++ 
-     Mail: 384099319@qq.com 
-     Created Time: 2015年01月20日 14:25:15
- ************************************************************************/
-#include<cstring>
 #include<cstdio>
 #include<algorithm>
 using namespace std;
@@ -21,34 +13,27 @@ int dist(int x,int y){
 
 void dfs(int x,int y,int steps){
   if(flag)return;
-  if(x==n*z1+x1&&y==y1){flag=1,ans=steps;return;}
+  if(x==n*z1+x1&&y==y1){
+    flag=1,ans=steps;
+    return;
+  }
   dis=dist(x,y);
   if(dis+steps>di)return;
   for(int i=0;i<4;i++){
-  //puts("gege");
     int dx=x+mx[i],dy=y+my[i],dsteps=steps+1;
-    if(mp[dx][dy]=='O'&&dx>=0&&dx<n*n&&dy>=0&&dy<n){
-      //printf("%c\n",mp[dx][dy]);
-      //printf("dx=%d,dy=%d,dsteps=%d\n",dx,dy,dsteps);
+    if(mp[dx][dy]=='O'&&dx>=0&&dx<n*n&&dy>=0&&dy<n)
       mp[dx][dy]='X',dfs(dx,dy,dsteps);
-    }
   }
 }
 
 int main(){
   int i,j;
   while(~scanf("%*s%d",&n)){
-    //printf("%d\n",n);
     flag=0;
-    for(i=0;i<n*n;i++)
-      scanf("%s",mp[i]);
-      //printf("%s\n",mp[j]);
+    for(i=0;i<n*n;i++) scanf("%s",mp[i]);
     scanf("%d%d%d",&y0,&x0,&z0);
     scanf("%d%d%d%*s",&y1,&x1,&z1);
-    //printf("%d %d %d\n%d %d %d\n",y0,x0,z0,y1,x1,z1);
     di=max(x1+n*z1-x0-n*z0,x0+z0*n-x1-n*z1)+max(y1-y0,y0-y1);
-    //printf("start%d %d end%d %d\n",x0+z0*n,y0,x1+z1*n,y1);
-    //printf("di=%d\n",di);
     dfs(x0+z0*n,y0,0);
     if(flag)printf("%d %d\n",n,ans);
     else puts("NO ROUTE");
