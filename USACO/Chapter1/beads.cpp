6e519200c4ee617176a18a0f00cc8479b0dc6fb3
obsolescenceL <384099319@qq.com>
2015-01-21 13:51:43 +0800

    add

---
+++ USACO/Chapter1/beads.cpp
@@ -0,0 +1,45 @@
+/*************************************************************************
+     File Name: beads.cpp
+     ID: obsoles1
+     PROG: beads
+     LANG: C++ 
+     Mail: 384099319@qq.com 
+     Created Time: 2015年01月21日  8:55:11
+ ************************************************************************/
+#include<cstdio>
+#include<string>
+#include<iostream>
+using namespace std;
+  
+int main(){
+  freopen("beads.out","w",stdout);
+  freopen("beads.in","r",stdin);
+  int n,i,j,change,cnt,max_cnt;
+  string s,ss;
+  while(~scanf("%d",&n)){
+    ss.clear();
+    cin>>ss;
+    //cout<<ss<<endl;
+    max_cnt=0;
+    for(i=0;i<n;i++){
+      s=ss;
+      change=cnt=0;
+      for(j=i;change<2;j++){
+        if(j==n)j=0;
+        if(s[j]=='w')s[j]=s[j+1];
+        if(j==n-1&&s[0]=='w')s[0]=s[j];
+        if(s[j+1]=='w')s[j+1]=s[j];
+        if(s[j]!=s[j+1]&&j!=n-1)change++;
+        if(s[j]!=s[0]&&j==n-1)change++;
+        if(cnt++>=n)break;
+        //cout<<j<<s[j]<<" "<<change<<' ';
+      }
+      //cout<<endl;
+      //printf("cnt=%d\n",cnt);
+      if(cnt>max_cnt)max_cnt=cnt;
+      if(max_cnt>=n)break;
+    }
+    if(max_cnt>=n)printf("%d\n",n);
+    else printf("%d\n",max_cnt);
+  }
+}
