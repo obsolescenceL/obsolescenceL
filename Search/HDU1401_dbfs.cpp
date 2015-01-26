b88b027e835be77531ae8a923bce6763e560fd54
obsolescenceL <384099319@qq.com>
2015-01-26 09:18:13 +0800

    add

--- Search/HDU1401_dbfs.cpp
+++ Search/HDU1401_dbfs.cpp
@@ -11,7 +11,8 @@
 #include<cstdio>
 #include<cstring>
 using namespace std;
-char vis[8][8][8][8][8][8][8][8];
+char vis[16777777];
+int num[]={1,8,64,512,4096,32768,262144,2097152};
 int mx[]={0,0,1,-1},my[]={1,-1,0,0};
 struct point{
   int x,y;
@@ -22,39 +23,35 @@ struct node{
 }s[2];
 
 int cmp(point p,point q){
-  if(p.x==q.x)return p.y<q.y;
-  return p.x<q.x;
+  return (p.x==q.x&&p.y<q.y)||p.x<q.x;
 }
 
-void set1(node t){
-  vis[t.p[0].x][t.p[0].y][t.p[1].x][t.p[1].y][t.p[2].x][t.p[2].y][t.p[3].x][t.p[3].y]='1';
-}
-
-void set2(node t){
-  vis[t.p[0].x][t.p[0].y][t.p[1].x][t.p[1].y][t.p[2].x][t.p[2].y][t.p[3].x][t.p[3].y]='2';
+int Hash(node a){
+  sort(a.p,a.p+4,cmp);
+  int sum=0,i;
+  for(i=0;i<4;i++){
+    sum+=a.p[i].x*num[i*2];
+    sum+=a.p[i].y*num[i*2+1];
+  }
+  return sum;
 }
 
 char isvisit(node t){
-  return vis[t.p[0].x][t.p[0].y][t.p[1].x][t.p[1].y][t.p[2].x][t.p[2].y][t.p[3].x][t.p[3].y];
+  return vis[Hash(t)];
 }
 
 int judge(node &t,int i,int j,int flag){
   if(flag){
-    if(t.step>=4) 
-      return 0;
+    if(t.step>=4) return 0;
     t.step++;    
   }
-  t.p[i].x+=mx[j];
-  t.p[i].y+=my[j];
+  t.p[i].x+=mx[j], t.p[i].y+=my[j];
   if(t.p[i].x>=0&&t.p[i].x<8&&t.p[i].y>=0&&t.p[i].y<8){
     int k;
-    for(k=0;k<4;k++){
-      if(i!=k){
-        if(t.p[i].x==t.p[k].x&& t.p[i].y==t.p[k].y)    
+    for(k=0;k<4;k++)
+      if(i!=k&&t.p[i].x==t.p[k].x&& t.p[i].y==t.p[k].y)    
           if(flag) return judge(t,i,j,0); 
           else return 0;    
-      }    
-    }
     if(k>=4){
       sort(t.p,t.p+4,cmp);
       return 1;
@@ -68,7 +65,9 @@ bool dbfs(){
   node t;
   int i,j,k;
   q[0].push(s[0]),q[1].push(s[1]);
-  set1(s[0]),set2(s[1]);
+  int Hashs[2];
+  for(i=0;i<2;i++)Hashs[i]=Hash(s[i]),vis[Hashs[i]]='1'+i;
+  if(Hashs[0]==Hashs[1])return 1;
   while(!q[0].empty()||!q[1].empty()){
     for(k=0;k<2;k++){
       if(q[k].empty())continue;
@@ -79,8 +78,7 @@ bool dbfs(){
             if(isvisit(t)-'1'==k)continue;
             if(isvisit(t)-'1'==1-k)return 1;
             q[k].push(t);
-            if(k)set2(t);
-            else set1(t);
+            vis[Hash(t)]='1'+k;
           }
         }
       q[k].pop();
@@ -99,6 +97,6 @@ int main(){
     s[0].step=s[1].step=0;
     sort(s[0].p,s[0].p+4,cmp);
     sort(s[1].p,s[1].p+4,cmp);
-    dbfs()? puts("YES"):puts("NO");
+    puts(dbfs()? "YES":"NO");
   }
 }
