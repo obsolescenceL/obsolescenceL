36cc626f02dded854fe387583e951fce562c2640
obsolescenceL <384099319@qq.com>
2015-01-27 16:23:53 +0800

    add

--- MinimumSpanningTree/HDU1863.cpp
+++ MinimumSpanningTree/HDU1863.cpp
@@ -7,46 +7,50 @@
  ************************************************************************/
 #include<cstdio>
 #include<algorithm>
+#include<iostream>
 using namespace std;
-const int MAXN=110;
+const int MAXN=100;
 int pre[MAXN];
-int n,m;
+int n,m,nn;
 struct Edge{
   int a,b,c;
+  bool operator < (const Edge& rhs)const {
+    return c<rhs.c;
+  }
 }edge[MAXN];
 
-bool operator < (const Edge& lhs,const Edge& rhs){
-  return lhs.c<rhs.c;
+
+inline void init(){
+  for(int i=1;i<=m;i++)pre[i]=i;
 }
 
-int init(){
-  for(int i=0;i<m;i++)pre[i]=i;
+inline int find(int x){
+  return x==pre[x]? x:pre[x]=find(pre[x]);
 }
 
-int find(int x){
-  x==pre[x]? x:pre[x]=find(pre[x]);
+inline void Union(int x,int y){
+  pre[find(y)]=find(x);
+  //cout<<find(y)<<endl;
 }
 
-void Union(int x,int y){
-  pre[find(x)]=find(y);
-}
-
-void Kruskal(){
+inline void Kruskal(){
   init();
   sort(edge,edge+n);
   int i,j,sum=0;
-  for(i=0,j=0;i<m-1&&j<n;i++){
-    while(find(edge[j].a)==find(edge[j].b))j++;
-    Union(edge[j].a,edge[j].b);
-    sum+=edge[j].c;
-    j++;
+  for(j=0;j<n;j++){
+    if(find(edge[j].a)!=find(edge[j].b)){
+      Union(edge[j].a,edge[j].b);
+      sum+=edge[j].c;
+      nn--;
+    }
   }
-  if(i==m-1)printf("%d\n",sum);
+  if(nn==1)printf("%d\n",sum);
   else puts("?");
 }
 
 int main(){
   while(~scanf("%d%d",&n,&m)&&n){
+    nn=m;
     for(int i=0;i<n;i++)
       scanf("%d%d%d",&edge[i].a,&edge[i].b,&edge[i].c);
     Kruskal();
