f6d633f930b710dcaa590fa9d7ff2b0dcc524f05
obsolescenceL <384099319@qq.com>
2015-03-29 10:04:12 +0800

    add

--- GoogleCodeJam/FileFix-it.cpp
+++ GoogleCodeJam/FileFix-it.cpp
@@ -19,7 +19,7 @@
 #include<set>
 using namespace std;
 string s,s1;
-set<string> mp;
+set<string> myset;
 
 int main(){
   int t,n,m,i,j;
@@ -27,19 +27,19 @@ int main(){
   while(cin>>t){
     for(int ncase=1;ncase<=t;++ncase){
       cin>>n>>m;
-      //mp.clear();
+      myset.clear();
       for(i=0;i<n;++i){
         cin>>s1;
         s.clear();
         s+=s1[0];
         for(j=1;s1[j];++j){
           if(s1[j]=='/'){
-            mp.insert(s);
+            myset.insert(s);
             //cout<<s<<endl;
           }
           s+=s1[j];
         }
-        mp.insert(s);
+        myset.insert(s);
       }
       int cnt=0;
       for(i=0;i<m;++i){
@@ -48,18 +48,18 @@ int main(){
         s+=s1[0];
         for(j=1;s1[j];++j){
           if(s1[j]=='/'){
-            if(!mp.count(s)){
+            if(!myset.count(s)){
               cnt++;
               //cout<<s<<endl;
-              mp.insert(s);
+              myset.insert(s);
             }
             //cout<<"cnt="<<cnt<<endl;
           }
           s+=s1[j];
         }
-        if(!mp.count(s)){
+        if(!myset.count(s)){
           cnt++;
-          mp.insert(s);
+          myset.insert(s);
         }
       }
       printf("Case #%d: %d\n",ncase,cnt);
