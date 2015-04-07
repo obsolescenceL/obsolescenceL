ed816763cb8fa130a81c3ebbcebe16668b5e8aba
obsolescenceL <384099319@qq.com>
2015-04-07 18:32:59 +0800

    add

--- team/4_6d.cpp
+++ team/4_6d.cpp
@@ -8,33 +8,21 @@
  ************************************************************************/
 #include<cstdio>
 #include<cstring>
-#include<iostream>
-#include<algorithm>
-#include<map>
 #include<queue>
-#include<stack>
-#include<cmath>
-#include<cctype>
-#include<ctime>
-#include<cstdlib>
-#include<string>
-#include<vector>
-#include<set>
-#include<bitset>
 #define Max(x,y) ((x)>(y)?(x):(y))
 #define Min(x,y) ((x)<(y)?(x):(y))
 #define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
 #define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
 using namespace std;
 const int INF=0x3f3f3f3f;
-char mp[55][55];
+char mp[51][51];
 int mx[]={0,0,1,-1},my[]={1,-1,0,0},n,m;
-bool vis[55][55];
+bool vis[51][51];
 
 struct Transfer{
   int x1,y1,x2,y2;
   bool flag;
-}trans[30];
+}trans[26];
 
 struct Status{
   int x,y,step;
@@ -72,19 +60,18 @@ void bfs(){
       }
     }
   }
-  if(flag)cout<<"-1\n";
-  else cout<<ans<<'\n';
+  if(flag)puts("-1");
+  else printf("%d\n",ans);
 }
 
 int main(){
   int t,i,j,x;
-  ios::sync_with_stdio(0);
-  while(cin>>t){
+  while(~scanf("%d",&t)){
     while(t--){
       memset(trans,0,sizeof trans);
-      cin>>n>>m;
-      for(i=0;i<n;++i)cin>>mp[i];
-      for(i=0;i<n;++i)
+      scanf("%d%d",&n,&m);
+      for(i=0;i<n;++i){
+        scanf("%s",mp[i]);
         for(j=0;j<m;++j){
           x=mp[i][j]-'a';
           if(mp[i][j]=='L')s.x=i,s.y=j,s.step=0;
@@ -93,6 +80,7 @@ int main(){
             else trans[x].x1=i,trans[x].y1=j,trans[x].flag=1;
           }
         }
+      }
       bfs();
     }
   }
