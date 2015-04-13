f5df17fefb09ca44e5c892d518440b398a6f2db5
obsolescenceL <384099319@qq.com>
2015-04-13 10:30:45 +0800

    add

--- DataStructure/SegmentTree/HDU1166.cpp
+++ DataStructure/SegmentTree/HDU1166.cpp
@@ -9,75 +9,64 @@
 #include<cstring>
 #include<iostream>
 using namespace std;
-const int MAXN=50010;
-int a[MAXN],ans,tree[MAXN<<2];
+const int N=50010;
+int a[N];
+struct node{
+  int sum,add;
+}tree[N<<2];
+
+inline void push_up(int t){
+  tree[t].sum=tree[t<<1].sum+tree[t<<1|1].sum;
+}
+
+inline void push_down(int t,int L,int R){
+  if(!tree[t].add)return;
+  int mid=(L+R)>>1;
+  tree[t<<1].sum+=tree[t].add*(mid-L+1);
+  tree[t<<1|1].sum+=tree[t].add*(R-mid);
+  tree[t<<1].add+=tree[t].add;
+  tree[t<<1|1].add+=tree[t].add;
+  tree[t].add=0;
+}
 
 inline void build(int t,int L,int R){
   if(L==R){
-    tree[t]=a[L];
+    tree[t].sum=a[L];
+    tree[t].add=0;
     return;
   }
-  for(int i=L;i<=R;++i)tree[t]+=a[i];
   int mid=(L+R)>>1;
   build(t<<1,L,mid);
   build(t<<1|1,mid+1,R);
+  push_up(t);
 }
-
-//void pull(int t,int l,int r){
-//  if(tree[t].add&&t){
-//    tree[t>>1].sum+=((l+r)<<1)*tree[t].add;
-//    tree[t>>1].add=tree[t].add;
-//    tree[t].add=0;
-//    pull(t>>1,l,r);
-//  }
-//}
-//
-//void push(int t,int l,int r){
-//  if(tree[t].add){
-//    tree[t<<1].sum+=(((l+r)>>1+l)>>1)*tree[t].add;
-//    tree[t<<1].add=tree[t].add;
-//    tree[t<<1|1].sum+=(((l+r)>>1|1+r)>>1)*tree[t].add;
-//    tree[t<<1|1].add=tree[t].add;
-//    tree[t].add=0;
-//    push(t<<1,l,(l+r)>>1);
-//    push(t<<1|1,(l+r)>>1|1,r);
-//  }
-//}
 
 inline void update(int t,int L,int R,int l,int r,int c){
-  if(L==l&&R==r){
-    tree[t]+=(R-L+1)*c;
+  if(L>=l&&R<=r){
+    tree[t].sum+=(R-L+1)*c;
+    tree[t].add+=c;
     return;
   }
-  //push(t,l,r);
+  push_down(t,L,R);
   int mid=(R+L)>>1;
-  if(r<=mid)update(t<<1,L,mid,l,r,c);
-  else if(l>mid)update(t<<1|1,mid+1,R,l,r,c);
-  else{
-    update(t<<1,L,mid,l,r,c);
-    update(t<<1|1,mid+1,R,l,r,c);
-  }
-  //pull(t,l,r);
-  tree[t]=tree[t<<1]+tree[t<<1|1];
+  if(l<=mid)update(t<<1,L,mid,l,r,c);
+  if(r>mid)update(t<<1|1,mid+1,R,l,r,c);
+  push_up(t);
 }
 
-inline void query(int t,int L,int R,int l,int r){
-  if(L==l&&R==r){
-    //cout<<tree[t].sum<<endl;
-    ans+=tree[t];
-    return;
-  }
+inline int query(int t,int L,int R,int l,int r){
+  if(L>=l&&R<=r) return tree[t].sum;
+  push_down(t,L,R);
   int mid=(R+L)>>1;
-  if(r<=mid)query(t<<1,L,mid,l,r);
-  else if(l>mid)query(t<<1|1,mid+1,R,l,r);
-  else {
-    query(t<<1,L,mid,l,mid);
-    query(t<<1|1,mid+1,R,mid+1,r);
-  }
+  int ans=0;
+  if(l<=mid)ans+=query(t<<1,L,mid,l,r);
+  if(r>mid)ans+=query(t<<1|1,mid+1,R,l,r);
+  return ans;
 }
 
 int main(){
   int T,k,n,i,r,l;
+  char op[20];
   while(~scanf("%d",&T)){
     for(k=1;k<=T;++k){
       memset(tree,0,sizeof tree);
@@ -85,14 +74,11 @@ int main(){
       scanf("%d",&n);
       for(i=1;i<=n;++i) scanf("%d",a+i);
       build(1,1,n);
-      char op[20];
       while(scanf("%s",op)){
         if(op[0]=='E')break;
         scanf("%d%d",&l,&r);
         if(op[0]=='Q'){
-          ans=0;
-          query(1,1,n,l,r);
-          printf("%d\n",ans);
+          printf("%d\n",query(1,1,n,l,r));
         }else if(op[0]=='A')update(1,1,n,l,l,r);
         else if(op[0]=='S')update(1,1,n,l,l,-r);
       }
