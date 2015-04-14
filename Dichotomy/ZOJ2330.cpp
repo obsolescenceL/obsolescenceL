09f9c8fa61355d2a41b9fb4a2d57ebc09700920b
obsolescenceL <384099319@qq.com>
2015-04-14 22:54:04 +0800

    add

---
+++ Dichotomy/ZOJ2330.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+     File Name: ZOJ2330.cpp
+     ID: obsoles1
+     PROG: 
+     LANG: C++ 
+     Mail: 384099319@qq.com 
+     Created Time: 2015年04月06日 星期一 23时57分33秒
+ ************************************************************************/
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<cctype>
+#include<ctime>
+#include<cstdlib>
+#include<string>
+#include<vector>
+#include<set>
+#include<bitset>
+#define Max(x,y) ((x)>(y)?(x):(y))
+#define Min(x,y) ((x)<(y)?(x):(y))
+#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
+#define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
+using namespace std;
+
+
+int main(){
+  double a,b;
+  while(~scanf("%lf",&a)){
+
