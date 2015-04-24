3ff2298e2ea5c31f0eb3e45085c8f8c34eb877a3
obsolescenceL <384099319@qq.com>
2015-04-24 11:28:23 +0800

    add

--- DP/DAG_dp/HDU4001.cpp
+++ DP/DAG_dp/HDU4001.cpp
@@ -8,27 +8,10 @@
  ************************************************************************/
 #include<cstdio>
 #include<cstring>
-#include<iostream>
-#include<algorithm>
-#include<map>
-#include<queue>
-#include<stack>
-#include<cmath>
-#include<cctype>
-#include<ctime>
-#include<cstdlib>
-#include<string>
 #include<vector>
-#include<set>
-#include<bitset>
 #define Max(x,y) ((x)>(y)?(x):(y))
-#define Min(x,y) ((x)<(y)?(x):(y))
-#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
-#define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
 #define ll long long
 #define Mem0(x) memset(x,0,sizeof(x))
-#define Mem1(x) memset(x,-1,sizeof(x))
-#define MemX(x) memset(x,0x3f,sizeof(x))
 #define pb push_back
 using namespace std;
 const int N=1001;
@@ -67,12 +50,9 @@ int main(){
       scanf("%d%d%d%d",&a,&b,&c,&d);
       if(a<b)swap(a,b);
       v.pb(block(a,b,c,d));
-      //v.pb(block(b,a,c,d,a*b));
     }
-    //sort(v.begin(),v.end());
     ll max_h=0;
     for(i=0;i<n;++i)
-      //dp[i]=v[i].h;
       for(j=0;j<i;++j){
         if(check(v[i],v[j]))g[i][j]=1;
         if(check(v[j],v[i]))g[j][i]=1;
