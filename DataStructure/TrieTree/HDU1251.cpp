be015e1a49ec9d4db723befff4e901fb4a522593
obsolescenceL <384099319@qq.com>
2015-06-07 21:30:30 +0800

    add

--- DataStructure/TrieTree/HDU1251.cpp
+++ DataStructure/TrieTree/HDU1251.cpp
@@ -7,24 +7,9 @@
      Created Time: 2015年04月09日 星期四 17时02分23秒
  ************************************************************************/
 #include<cstdio>
-#include<cstring>
+#include<string>
 #include<iostream>
 #include<algorithm>
-#include<map>
-#include<queue>
-#include<stack>
-#include<cmath>
-#include<cctype>
-#include<ctime>
-#include<cstdlib>
-#include<string>
-#include<vector>
-#include<set>
-#include<bitset>
-#define Max(x,y) ((x)>(y)?(x):(y))
-#define Min(x,y) ((x)<(y)?(x):(y))
-#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
-#define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
 using namespace std;
 const int branchNum=26;
 struct tree{
