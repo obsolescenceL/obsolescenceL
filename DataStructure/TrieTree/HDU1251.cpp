e6bb65da98918dc34972c3f15a1b2d09971b1874
obsolescenceL <384099319@qq.com>
2015-06-14 11:35:36 +0800

    add

--- DataStructure/TrieTree/HDU1251.cpp
+++ DataStructure/TrieTree/HDU1251.cpp
@@ -8,49 +8,58 @@
  ************************************************************************/
 #include<cstdio>
 #include<string>
+#include<cstring>
 #include<iostream>
 #include<algorithm>
+#define Mem0(x) memset(x,0,sizeof(x))
 using namespace std;
 const int branchNum=26;
 struct tree{
   int cnt;
   tree *next[branchNum];
-}root;
+}*root;
+
+tree *init(){
+  tree *temp;
+  temp=(tree *)malloc(sizeof(tree));
+  temp->cnt=0;
+  Mem0(temp->next);
+  return temp;
+}
 
 void build(string &word){
-  int i,j,x;
-  tree *p=&root,*temp;
-  for(i=0;word[i];++i){
-    x=word[i]-'a';
-    if(p->next[x]){
-      p=p->next[x];
-      (p->cnt)++;
-    }else{
-      temp=(tree *)malloc(sizeof root);
-      temp->cnt=1;
-      for(j=0;j<26;++j)
-        temp->next[j]=0;
-      p->next[x]=temp;
-      p=p->next[x];
-    }
+  int i=0,x;
+  tree *p=root;
+  while(word[i]){
+    x=word[i++]-'a';
+    if(!(p->next[x]))p->next[x]=init();
+    p=p->next[x];
+    (p->cnt)++;
   }
 }
 
 int search(string &word){
-  int i,x;
-  tree *p=&root,*temp;
-  for(i=0;word[i];++i){
-    x=word[i]-'a';
+  int i=0,x;
+  tree *p=root;
+  while(word[i]){
+    x=word[i++]-'a';
     if(!(p=p->next[x]))return 0;
   }
   return p->cnt;
 }
 
+void Del(tree *p){
+  for(int i=0;i<branchNum;++i)
+    if(p->next[i])Del(p->next[i]);
+  free(p);
+}
+
 int main(){
   string word;
-  for(int i=0;i<26;++i)root.next[i]=0;
+  root=init();
   while(getline(cin,word),word!="")
     build(word);
   while(cin>>word)
     printf("%d\n",search(word));
+  Del(root);
 }
