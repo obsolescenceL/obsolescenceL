6b77040093694ca3013e1c89124922edcf66bf2f
obsolescenceL <384099319@qq.com>
2015-08-08 16:33:03 +0800

    add

--- DataStructure/Treap/SPOJ_ORDERSET_2.cpp
+++ DataStructure/Treap/SPOJ_ORDERSET_2.cpp
@@ -7,8 +7,29 @@
      Created Time: 2015年08月07日 星期五 09时33分02秒
  ************************************************************************/
 #include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<cctype>
 #include<ctime>
 #include<cstdlib>
+#include<string>
+#include<vector>
+#include<set>
+#include<bitset>
+#define Max(x,y) ((x)>(y)?(x):(y))
+#define Min(x,y) ((x)<(y)?(x):(y))
+#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
+#define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
+#define ll long long
+#define Mem0(x) memset(x,0,sizeof(x))
+#define Mem1(x) memset(x,-1,sizeof(x))
+#define MemX(x) memset(x,0x3f,sizeof(x))
+#define pb push_back
 using namespace std;
 struct treap{
   treap *l,*r;
@@ -82,19 +103,36 @@ int q_count(treap *&p,int v,int cnt){
   else return q_count(p->r,v,cnt+p->l_size()+1);
 }
 
+int q_rank(treap *&p,int v,int cur){
+  //cout<<"p->v="<<p->v<<endl;
+  if(p->v == v)return p->l_size()+cur+1;
+  else if(p->v > v)return q_rank(p->l,v,cur);
+  else return q_rank(p->r,v,cur+p->l_size()+1);
+}
+
+treap *q_pred(treap *&p,int v,treap *optimal){
+  if(!p)return optimal;
+  if(p->v < v) return q_pred(p->r,v,p);
+  else return q_pred(p->l,v,optimal);
+}
+
 int main(){
   int q,x;
   char op[2];
   srand(time(0));
   while(~scanf("%d",&q)){
-    puts("Case");
     root=0;
     while(q--){
+      //cout<<"num="<<num<<endl;
       scanf("%s%d",op,&x);
+      //cout<<"op="<<op<<endl;
       if(op[0]=='I')insert(root,x);
       else  if(op[0]=='D')del(root,x);
       else if(op[0]=='C'){
-        printf("%d\n",q_count(root,x-1,0));
+        treap *optimal=q_pred(root,x,0);
+        if(root && root->size && optimal){
+          printf("%d\n",q_rank(root,optimal->v,0));
+        }else puts("0");
       }else if(op[0]=='K'){
         if(root && root->size >= x)printf("%d\n",find_kth(root,x)->v);
         else puts("invalid");
