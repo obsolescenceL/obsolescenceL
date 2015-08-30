8b240718bf64e6d95cf7f2c69317c5242d1efcf4
obsolescenceL <384099319@qq.com>
2015-08-30 10:42:20 +0800

    add

---
+++ Water/HDU5422.cpp
@@ -0,0 +1,48 @@
+/*************************************************************************
+     File Name: A.cpp
+     ID: obsoles1
+     PROG: 
+     LANG: C++ 
+     Mail: 384099319@qq.com 
+     Created Time: 2015年08月29日 星期六 19时02分29秒
+ ************************************************************************/
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<cctype>
+#include<ctime>
+#include<cstdlib>
+#include<string>
+#include<vector>
+#include<set>
+#include<bitset>
+#define Max(x,y) ((x)>(y)?(x):(y))
+#define Min(x,y) ((x)<(y)?(x):(y))
+#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
+#define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
+#define ll long long
+#define Mem0(x) memset(x,0,sizeof(x))
+#define Mem1(x) memset(x,-1,sizeof(x))
+#define MemX(x) memset(x,0x3f,sizeof(x))
+#define pb push_back
+using namespace std;
+const int N=110;
+bool adj[N][N];
+
+int main(){
+  int n,m,i,u,v;
+  while(~scanf("%d%d",&n,&m)){
+    Mem0(adj);
+    while(m--){
+      scanf("%d%d",&u,&v);
+      adj[u][v]=adj[v][u]=1;
+    }
+    if(!adj[1][n])puts("1 1");
+    else printf("1 %d\n",n*(n-1)/2);
+  }
+}
