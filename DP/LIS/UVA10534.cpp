27bf8f3061a3b6d90e1881253a4193bbd02dd82a
obsolescenceL <384099319@qq.com>
2015-11-08 16:19:21 +0800

    LWS

--- DP/LIS/UVA10534.cpp
+++ DP/LIS/UVA10534.cpp
@@ -1,39 +1,64 @@
 /*************************************************************************
-    > File Name: UVA10534.cpp
-    > Author: obsolescence
-    > Mail: 384099319@qq.com 
-    > Created Time: 2015年01月 3日 21:59:35
+     File Name: J.cpp
+     ID: obsoles1
+     PROG: 
+     LANG: C++ 
+     Mail: 384099319@qq.com 
+     Created Time: 日 11/ 8 15:56:22 2015
  ************************************************************************/
 #include<cstdio>
+#include<cstring>
+#include<iostream>
 #include<algorithm>
+#include<map>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<cctype>
+#include<ctime>
+#include<cstdlib>
+#include<string>
+#include<vector>
+#include<set>
+#include<bitset>
+#define Max(x,y) ((x)>(y)?(x):(y))
+#define Min(x,y) ((x)<(y)?(x):(y))
+#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
+#define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
+#define ll long long
+#define Mem0(x) memset(x,0,sizeof(x))
+#define Mem1(x) memset(x,-1,sizeof(x))
+#define MemX(x) memset(x,0x3f,sizeof(x))
+#define pb push_back
 using namespace std;
-const int MAXN=10010;
-int dp_lis[MAXN],dp_lds[MAXN],a[MAXN];
+const int N=10010;
+int lis[N],lds[N],a[N],v[N];
+
 int main(){
-  int n,i,j;
+  int i,j,n;
   while(~scanf("%d",&n)){
-    for(i=0;i<n;i++){
+    for(i=0;i<n;++i){
       scanf("%d",a+i);
-      dp_lis[i]=1;
-      dp_lds[i]=1;
+      lis[i]=lds[i]=1;
     }
-    int max_lis=0,max_lds=0,v[MAXN],pos;
-    for(i=0;i<n;i++){
-      pos=lower_bound(v,v+max_lis,a[i])-v;
-      dp_lis[i]=pos+1;
+    int pos,max_l=0;
+    for(i=0;i<n;++i){
+      pos=lower_bound(v,v+max_l,a[i])-v;
+      lis[i]=pos+1;
       v[pos]=a[i];
-      max_lis=max(max_lis,pos+1);
+      max_l=Max(max_l,pos+1);
     }
-    for(i=n-1;i>=0;i--){
-      pos=lower_bound(v,v+max_lds,a[i])-v;
-      dp_lds[i]=pos+1;
+    max_l=0;
+    for(i=n-1;i>=0;--i){
+      pos=lower_bound(v,v+max_l,a[i])-v;
+      lds[i]=pos+1;
       v[pos]=a[i];
-      max_lds=max(max_lds,pos+1);
+      max_l=Max(max_l,pos+1);
     }
-    //for(i=0;i<n;i++) printf("dp_lis[%d]=%d,dp_lds[%d]=%d\n",i,dp_lis[i],i,dp_lds[i]);
-    int ans=0;
-    for(i=0;i<n;i++)ans=max(ans,min(dp_lis[i],dp_lds[i]));
-    printf("%d\n",ans*2-1);
+    int lws=0;
+    for(i=0;i<n;++i)
+      lws=Max(lws,Min(lis[i],lds[i]));
+    lws=lws*2-1;
+    printf("%d\n",lws);
   }
 }
-
