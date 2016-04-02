6209c2264f0c442333eb3d5c863aeff0991f5535
obsolescenceL <384099319@qq.com>
2016-04-02 22:25:31 +0800

    add

---
+++ training/bc_16_4_2/HDU5655.cpp
@@ -0,0 +1,49 @@
+/*************************************************************************
+     File Name: A.cpp
+     ID: obsoles1
+     PROG: 
+     LANG: C++ 
+     Mail: 384099319@qq.com 
+     Created Time: 六  4/ 2 19:00:25 2016
+ ************************************************************************/
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<cctype>
+#include<ctime>
+#include<cstdlib>
+#include<string>
+#include<vector>
+#include<set>
+#include<bitset>
+#define Max(x,y) ((x)>(y)?(x):(y))
+#define Min(x,y) ((x)<(y)?(x):(y))
+#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
+#define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
+#define ll long long
+#define Mem0(x) memset(x,0,sizeof(x))
+#define Mem1(x) memset(x,-1,sizeof(x))
+#define MemX(x) memset(x,0x3f,sizeof(x))
+#define pb push_back
+using namespace std;
+
+int main(){
+  ll a[10];
+  int t,i;
+  ios::sync_with_stdio(0);
+  while(cin>>t){
+    while(t--){
+      for(i=0;i<4;++i){
+        cin>>a[i];
+      }
+      sort(a,a+4);
+      if(a[0] && a[3]-a[1]-a[0]<a[2]) cout<<"Yes\n";
+      else cout<<"No\n";
+    }
+  }
+}
