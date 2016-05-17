a93171110ba0e8e2edd8e36b17fb2f574e6d00e3
obsolescenceL <384099319@qq.com>
2016-05-17 21:06:25 +0800

    add

--- DataStructure/SegmentTree/UESTC1324.cpp
+++ DataStructure/SegmentTree/UESTC1324.cpp
@@ -8,20 +8,12 @@
 ************************************************************************/
 #include<cstdio>
 #include<cstring>
-#include<iostream>
 #include<algorithm>
 #define Max(x,y) ((x)>(y)?(x):(y))
-#define Min(x,y) ((x)<(y)?(x):(y))
-#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
-#define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
 #define ll long long
 #define Mem0(x) memset(x,0,sizeof(x))
-#define Mem1(x) memset(x,-1,sizeof(x))
-#define MemX(x) memset(x,0x3f,sizeof(x))
-#define pb push_back
-//using namespace std;
 const int N=100010;
-int tree[N<<2];
+int tree[N<<2],ans;
 
 template<class T>
 inline bool read(T &n){
@@ -55,36 +47,37 @@ void push_up(int t){
 
 void update(int t,int L,int R,int x,int v){
   if(L==R){tree[t]+=v;return;}
-  int mid=(R+L)>>1;
+  int mid=(L+R)>>1;
   if(x<=mid)update(t<<1,L,mid,x,v);
-  else update(t<<1|1,mid+1,R,x,v);
+  if(x>mid)update(t<<1|1,mid+1,R,x,v);
   push_up(t);
 }
 
-int query(int t,int L,int R,int l,int r){
-  if(l<=L && R<=r)return tree[t];
+void query(int t,int L,int R,int l,int r){
+  if(l<=L && R<=r){
+    ans=Max(ans,tree[t]);
+    return;
+  }
   int mid=(L+R)>>1;
-  int ans=0;
-  if(l<=mid)ans=Max(ans,query(t<<1,L,mid,l,r));
-  if(r>mid)ans=Max(ans,query(t<<1|1,mid+1,R,l,r));
-  return ans;
+  if(l<=mid)query(t<<1,L,mid,l,r);
+  if(r>mid)query(t<<1|1,mid+1,R,l,r);
 }
 
 int main(){
-  int n,q,t,p,x,ans;
-  //while(~scanf("%d%d",&n,&q)){
-  while(read(n) && read(q)){
-    Mem0(tree);
-    while(q--){
-      //scanf("%d%d%d",&t,&p,&x);
-      read(t),read(p),read(x);
-      if(t==1)update(1,1,n,p,x);
-      else if(t==2){
-        ans=query(1,1,n,p,x);
-        write(ans);
-        putchar('\n');
-        //printf("%d\n",query(1,1,n,p,x));
-      }
+  int n,q,t,p,x;
+  //scanf("%d%d",&n,&q);
+  read(n),read(q);
+  while(q--){
+    //scanf("%d%d%d",&t,&p,&x);
+    read(t),read(p),read(x);
+    if(t==1){
+      update(1,1,n,p,x);
+    }else if(t==2){
+      ans=0;
+      query(1,1,n,p,x);
+      //printf("%d\n",ans);
+      write(ans);
+      putchar('\n');
     }
   }
 }
