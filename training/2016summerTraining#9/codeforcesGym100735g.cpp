f5de8df731e9f6a10eca964f7672ff2bfc1ebda1
obsolescenceL <384099319@qq.com>
2016-08-13 14:36:14 +0800

    add

---
+++ training/2016summerTraining#9/codeforcesGym100735g.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************
+     File Name: G.cpp
+     ID: obsolescence
+     BLOG: http://blog.csdn.net/obsolescence
+     LANG: C++ 
+     Mail: 384099319@qq.com 
+     Created Time: 2016年08月01日 星期一 13时29分53秒
+ ************************************************************************/
+#include<bits/stdc++.h>
+#define Max(x,y) ((x)>(y)?(x):(y))
+#define Min(x,y) ((x)<(y)?(x):(y))
+#define each(it,v) for(__typeof((v).begin()) it=(v).begin();it!=(v).end();++it)
+#define Abs(x,y) ((x)>(y)?((x)-(y)):((y)-(x)))
+#define ll long long
+#define Mem0(x) memset(x,0,sizeof(x))
+#define Mem1(x) memset(x,-1,sizeof(x))
+#define MemX(x) memset(x,0x3f,sizeof(x))
+#define pb push_back
+using namespace std;
+
+int main() {
+  string s;
+  while (cin>>s) {
+    int cnt=0;
+    for (int i=0; s[i]; ++i)
+      if (s[i]=='1') cnt++;
+    cout<<Min(cnt,s.size()-cnt)<<endl;
+  }
+}
